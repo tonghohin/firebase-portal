e65d1fa0a29bc218c6e66fdacba707d7741b76d3
Hin <tonghohin77@gmail.com>
2023-02-06 23:10:58 -0400

    updated

--- server/firebase/config.js
+++ server/firebase/config.js
@@ -1,9 +1,10 @@
+const path = require("path");
+require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
 const { initializeApp, cert } = require("firebase-admin/app");
 const { getAuth } = require("firebase-admin/auth");
 const { getFirestore } = require("firebase-admin/firestore");
-const serviceAccount = require("./adminSDK.json");
 
-const admin = initializeApp({ credential: cert(serviceAccount) });
+const admin = initializeApp({ credential: cert(JSON.parse(process.env.ADMIN_SDK)) });
 const auth = getAuth(admin);
 const db = getFirestore(admin);
 
