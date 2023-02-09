085e89ca32b9328203337410e5eca647de46566a
Hin <tonghohin77@gmail.com>
2023-02-09 10:37:40 -0400

    cron

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ app.listen(process.env.PORT || 8000, () => {
   console.log("SERVER LISTENING!");
 });
 
-const options = {
+const OPTIONS = {
   dotfiles: "ignore",
   etag: false,
   extensions: ["htm", "html", "css", "js", "ico", "jpg", "jpeg", "png", "svg"],
@@ -14,11 +14,11 @@ const options = {
   maxAge: "1m",
   redirect: false
 };
-app.use(express.static("build", options));
+app.use(express.static("build", OPTIONS));
 app.use(express.static(path.join(__dirname, "../client/build")));
 app.use(express.json());
 
-// ------------------------------ Schedule Cron Job ------------------------------
+// ------------------------------ Scheduled Cron Job ------------------------------
 const Cron = require("./route/cron");
 app.use(Cron);
 
@@ -37,6 +37,7 @@ app.use("/admin", Register);
 const UserGym = require("./route/user/gym");
 app.use("/", UserGym);
 
+// ------------------------------ For React ------------------------------
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build", "index.html"));
 });
