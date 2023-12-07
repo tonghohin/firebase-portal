5005c8cbd3b49696bd4dc15bc9c9a8589f17c814
Hin <tonghohin77@gmail.com>
2023-12-06 23:52:30 -0400

    Remove unused file

---
+++ client/src/firebase/apiKey.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBSC4NRvgHwed4zNLjt_4rOcrJSsVb-ouc",
+    authDomain: "portal-2d632.firebaseapp.com",
+    projectId: "portal-2d632",
+    storageBucket: "portal-2d632.appspot.com",
+    messagingSenderId: "613542925486",
+    appId: "1:613542925486:web:fbaab57e7179f82de1093f",
+    measurementId: "G-F4MY4VHKJN"
+};
+
+export { firebaseConfig };
